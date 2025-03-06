3344c0be974be4d137d3605953005b593a88dc37
franciscocumbrera <francisco.cumbrera@alu.uhu.es>
2025-03-06 08:40:00 +0100

    Update wedding_woes.pl

--- exercisn/wedding_woes.pl
+++ exercisn/wedding_woes.pl
@@ -8,3 +8,6 @@ likes(gustavo, valeria).
 
 pairing(A, _):- chatty(A).
 pairing(_, A):- chatty(A).
+pairing(A,B:-likes(A,B),likes(B,A).
+
+seating(A,B,C,D,E):-pairing(A,B),pairing(B,C),pairing(C,D),pairing(D,E).
