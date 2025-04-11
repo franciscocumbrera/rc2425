20553e3a3537a3210a29736be81923a85d26aba0
franciscocumbrera <francisco.cumbrera@alu.uhu.es>
2025-04-11 13:29:22 +0200

    Update rna_transcription.pl

--- exercisn/rna_transcription.pl
+++ exercisn/rna_transcription.pl
@@ -1,9 +1,23 @@
+
 /*
 rna_transcription(Dna, Rna).
+unifica si RNA es la transcripcion de DNA
 */
 
-rna_aux("G","C").
-rna_aux("C","G").
-rna_aux("T","A").
-rna_aux("A","U").
-rna_transcription(Dna, Rna) :- rna_aux(Dna, Rna).
+rna_aux('G', 'C').
+rna_aux('C', 'G').
+rna_aux('T', 'A').
+rna_aux('A', 'U').
+
+rna_transcription("", "").
+
+rna_transcription(Dna, Rna) :-
+    string_chars(Dna, DnaList),        
+    transcribe_list(DnaList, RnaList), 
+    string_chars(Rna, RnaList).        
+
+transcribe_list([], []).
+
+transcribe_list([Cab|Resto], [X|Res]) :-
+    rna_aux(Cab, X),
+    transcribe_list(Resto, Res).
