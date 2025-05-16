8b4b14b132c611af331ac5217e56ccc656b81872
franciscocumbrera <francisco.cumbrera@alu.uhu.es>
2025-05-16 10:13:38 +0200

    Update hamming.pl

--- exercisn/hamming.pl
+++ exercisn/hamming.pl
@@ -1,1 +1,10 @@
+hamming_distance(Str1, Str2, Dist) :-
+    string_chars(Str1, L1),
+    string_chars(Str2, L2),
+    maplist(diff, L1, L2, Diff),
+    sum_list(Diff, Dist).
+
+    diff(C1, C2, D) :- 
+        C1 = C2 -> D = 0; D = 1.
+
 
